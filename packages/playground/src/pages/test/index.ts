9e86c885663f98fa527d25e339d5a2d560f3475f
susanforme <rzc307853639@gmail.com>
2026-04-16 16:30:44 +0800

    refactor: simplify TestPage styles by removing unused CSS rules

--- packages/playground/src/pages/test/index.ts
+++ packages/playground/src/pages/test/index.ts
@@ -5,33 +5,10 @@ import { defineLitRoute } from 'vite-plugin-lit-ssg'
 @customElement('test-page')
 export class TestPage extends LitElement {
   static styles = css`
-    :host {
-      display: block;
-      font-family: sans-serif;
-      max-width: 800px;
-      margin: 0 auto;
-      padding: 2rem;
-    }
-
-    h1 {
-      color: #333;
-    }
-
-    nav a {
-      margin-right: 1rem;
-      color: #0066cc;
-      text-decoration: none;
-    }
-
-    nav a:hover {
-      text-decoration: underline;
-    }
-
-    ul {
-      line-height: 1.8;
+    div{
+      color: red;
     }
   `
-
   render() {
     return html`
       <div>test</div>
