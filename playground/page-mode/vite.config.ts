cf694284560b71ba78de66dafebe5985e0417133
susanforme <rzc307853639@gmail.com>
2026-04-21 14:48:34 +0800

    feat: add jiti as a dependency and update paths in tsconfig for better module resolution

--- playground/page-mode/vite.config.ts
+++ playground/page-mode/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
-import { litSSG } from 'vite-plugin-lit-ssg'
+import {createJiti} from 'jiti'
 import Inspect from 'vite-plugin-inspect'
+const jiti = createJiti(import.meta.url)
+
+const { litSSG } = await jiti.import<typeof import('vite-plugin-lit-ssg')>('vite-plugin-lit-ssg')
 
 export default defineConfig({
   base:'./',
